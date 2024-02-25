f8903ceba0db90e26f4cb54cd2d293282baac441
Jack Lo Russo <jack.lorusso@gmail.com>
2024-02-25 13:49:58 +1100

    try commenting out webmentions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export { getFormattedDate } from "./date";
 export { elementHasClass, toggleClass } from "./domElement";
 export { sortMDByDate, getUniqueTags, getUniqueTagsWithCount } from "./post";
-export { getWebmentionsForUrl } from "./webmentions";
+// export { getWebmentionsForUrl } from "./webmentions";
 // export { agent } from "./bsky-agent";
