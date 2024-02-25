d791e1645d8afdfba09e2ae49670c6008370331a
Jack Lo Russo <jack.lorusso@gmail.com>
2024-02-25 12:04:13 +1100

    update feb 2024

--- src/utils/bsky-agent.ts
+++
@@ -1,9 +0,0 @@
-import * as atproto from "@atproto/api";
-
-export const agent = new atproto.BskyAgent({
-  // AppView URL
-  service: "https://api.bsky.app",
-});
-
-// https://github.com/withastro/astro/issues/3174
-// https://github.com/bluesky-social/atproto/issues/330#issuecomment-1536562004
